8d6bc9ae647f3544a7fe78c895184d6939f5d89e
kaurih <146112244+kaurih@users.noreply.github.com>
2025-03-02 01:56:24 +0200

    WS02b EX1

---
+++ WS02b/app.js
@@ -0,0 +1,15 @@
+//Monster Hunter World skill information from an open API
+
+const axios = require('axios');
+
+//Api endpoint
+const apiUrl= 'https://mhw-db.com/skills';
+
+//Get request to api
+axios.get(apiUrl)
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.error('Failed to GET skills:', error)
+    });
