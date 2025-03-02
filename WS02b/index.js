1a20943c2239f8ad93fa71865cdd19324ac39938
kaurih <146112244+kaurih@users.noreply.github.com>
2025-03-02 08:48:45 +0200

    added write script

--- WS02b/index.js
+++ WS02b/index.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
+const path = require('path');
 
-fs.readFile('./example.txt', 'utf8', (err, data) => {
+
+//read script
+fs.readFile(path.join(__dirname, 'example.txt'), 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
 })
+
+//write script
+fs.writeFile(path.join(__dirname, 'output.txt'), 'Tämä teksti menee output.txt -tiedostoon.', (err) => {
+    if (err) throw err;
+    console.log('Write script completed.');
+})
+
 
 // exit on uncaught errors
 process.on('uncaughtException', err => {
