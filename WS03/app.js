1cc7f8bffa744fce131b7b4d3b97f559eacdb1b3
kaurih <146112244+kaurih@users.noreply.github.com>
2025-03-15 18:22:03 +0200

    fixed GET requests

--- WS03/app.js
+++ WS03/app.js
@@ -7,15 +7,15 @@ server.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-server.get('/', (req, res) => {
+server.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
-server.get('/', (req, res) => {
+server.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'contact.html'));
 });
 
-server.get('/', (req, res) => {
+server.get('/services', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'services.html'));
 });
 
