0963661dad630fe37488bce8c1fc38771c079d89
kaurih <146112244+kaurih@users.noreply.github.com>
2025-03-23 21:46:39 +0200

    update

--- WS03/app.js
+++ WS03/app.js
@@ -4,17 +4,34 @@ const path = require('path');
 const PORT = 3000 || process.env.PORT;
 const parser = require('body-parser');
 
-// setting up body-parser middleware for JSON
-
+// Body-parser middleware setup
 server.use(parser.urlencoded({extended:true}));
 server.use(express.static('public'));
 
+//Logging middleware
+function logRequest(req, res, next){
+    console.log(`${req.method} ${req.url}`);
+    if (req.headers['x-custom-header']){
+        next();
+    } else {
+        res.status(400).send('Error: Custom header is missing!');
+    }    
+    
+}
+
+function checkHeader(req, res, next){
+
+}
+
+//POST
 server.post('/submit', (req, res) => {
     const requestBody= req.body;
     console.log(requestBody); // log the response to console
     res.json(requestBody); // respond with recieved data
 });
 
+
+//GET
 server.get('/submit', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'submit.html'));
 });
