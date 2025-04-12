bb3336553dd2356e48072ea1a07de0240462fce2
kaurih <146112244+kaurih@users.noreply.github.com>
2025-04-12 09:12:24 +0300

    up

---
+++ WS02b/readme.md
@@ -0,0 +1,16 @@
+# Tehtäväpalautus WS02b
+## Excercise 1
+
+## Excercise 2
+
+## Excercise 3
+
+## Excercise 4
+
+## Excercise 5
+
+## Excercise 6
+
+## Excercise 7
+
+## Excercise 8
--- WS03b/app.js
+++ WS03b/app.js
@@ -1,17 +1,1 @@
-const express = require('express');
-const parser = require('body-parser');
-const ejs = require('ejs');
-const path = require('path');
-
-var app = express();
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.render('index', {message: 'Hello world'});
-});
-
-
-//start the server with node app
-app.listen(3000, () => console.log(`Server running on port 3000`));
+i
