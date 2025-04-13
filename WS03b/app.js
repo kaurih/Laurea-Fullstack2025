1527c3ba4530af61387008a58aa46244723ee242
kaurih <146112244+kaurih@users.noreply.github.com>
2025-04-13 20:32:34 +0300

    exercise 2 ja css

--- WS03b/app.js
+++ WS03b/app.js
@@ -8,4 +8,8 @@ const port = 3000
 app.set('view engine', 'ejs');
 app.use('/', routes); // found in the routes directory
 
+
+//serve CSS
+app.use(express.static('public'));
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- WS03b/routes/routes.js
+++ WS03b/routes/routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 // Route to home page
+// Exercise 1: EJS setup
 
 router.get('/', (req, res) => {
     res.render('pages/index', {
         title: 'EJS-harjoittelua', 
-        message: 'Hello, EJS!'
+        message: 'Kakkuainekset',
+
+        //Exercise 2: a list of items
+        items: ['maitoa', 'munia', 'jauhoja', 'sokeria']
     });
 });
 
