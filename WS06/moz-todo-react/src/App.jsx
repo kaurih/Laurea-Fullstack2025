aba2c01f351a6d10e54bd045e3f81e0f8230c076
kaurih <146112244+kaurih@users.noreply.github.com>
2025-05-11 21:50:41 +0300

    WS06 lisätty

---
+++ WS06/moz-todo-react/src/App.jsx
@@ -0,0 +1,48 @@
+import { nanoid } from "nanoid";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
+import { useState } from "react";
+
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks?.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
+
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
